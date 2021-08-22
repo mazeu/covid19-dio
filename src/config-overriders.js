dc4de66caf89bcac49c7420291c219244a40ed23
mazeu <marciomazeu@hotmail.com>
2021-08-22 14:50:48 -0300

    First commit

---
+++ src/config-overriders.js
@@ -0,0 +1,3 @@
+const { useBabelRc, override } = require('customize-cra')
+// eslint-disable-next-line react-hooks/rules-of-hooks
+module.experts = override(useBabelRc())
---
+++ src/containers/index.jsx
@@ -0,0 +1,37 @@
+import React, { memo, useState, useCallback, useEffect } from 'react'
+import Api from '../api'
+import Board from './components/Board'
+
+import { ContainerStyled } from './style'
+
+function Main() {
+  const [data, setData] = useState({})
+  const [country, setCountry] = useState('brazil')
+  
+
+  const getCovidData = useCallback((country) => {
+    Api.getCountry(country)
+      .then(data => setData(data))
+  }, [])
+
+  useEffect(() => {    
+    getCovidData(country)
+  }, [getCovidData, country])
+
+  const handleChange = ({ target }) => {
+    const country = target.value
+    setCountry(country)
+  }
+
+  return (
+    <ContainerStyled>
+      <div className="mb-2">
+        
+      </div>
+      <Board data={data} />
+      
+    </ContainerStyled>
+  )
+}
+
+export default memo(Main)
